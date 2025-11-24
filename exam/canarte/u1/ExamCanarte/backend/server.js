59881f7555a463e839c15e598d7d083ef310f318
saraycanarteg <sacanarte@espe.edu.ec>
2025-11-23 22:11:21 -0500

    hw2 project uris

--- exam/canarte/u1/ExamCanarte/backend/server.js
+++ exam/canarte/u1/ExamCanarte/backend/server.js
@@ -1,29 +1,29 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const path = require('path');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const path = require("path");
 
-const clientRoutes = require('./routes/clients');
-
+const teamRoutes = require("./routes/team");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/clients', clientRoutes);
-
-
-app.use(express.static(path.join(__dirname, '../')));
-
+app.use("/api/team", teamRoutes);
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log(' Connected to MongoDB Atlas'))
-  .catch(err => console.error(' Connection Error:', err));
+    .then(() => console.log("Connected to MongoDB"))
+    .catch(err => console.error("MongoDB connection error:", err));
+
+app.use(express.static(path.join(__dirname, "..")));
 
 app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, '../', '/panels/clients.html'));
+    res.sendFile(path.join(__dirname, "..", "/panels/clients.html"));
 });
 
+const PORT = process.env.PORT || 5502;
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
+
