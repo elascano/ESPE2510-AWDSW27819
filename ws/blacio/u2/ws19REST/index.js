e4a1c1beca9338b3ca5176f0f1065202550c3364
Julio César Blacio Machuca <jcblaciomachuca@gmail.com>
2025-12-06 19:58:43 -0500

    ws folders v2.0

---
+++ ws/blacio/u2/ws19REST/index.js
@@ -0,0 +1,14 @@
+//Julio's port 3005
+const port = 3005;
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://oop:oop@cluster0.9knxc.mongodb.net/oop?retryWrites=true&w=majority&appName=Cluster0');
+
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('System connected to MongoDB Database'));
+app.use(express.json());
+const customerRoutes = require('./routes/customerRoutes');
+app.use('/computerstore', customerRoutes);
+app.listen(port, () => { console.log("Julio's Computer Store Server is running on port -->" + port)});
